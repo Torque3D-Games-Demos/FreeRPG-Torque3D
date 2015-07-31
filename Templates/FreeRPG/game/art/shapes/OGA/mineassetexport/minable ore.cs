25c7f8e318fee9daf0801e1ddc974f66b9fa50b9
Michael Haslam <hreikin@gmail.com>
2015-07-31 01:03:52 +0100

    remove renamed/replaced files

--- Templates/FreeRPG/game/art/shapes/OGA/mineassetexport/minable ore.cs
+++
@@ -1,8 +0,0 @@
-
-singleton TSShapeConstructor(Minable oreDae)
-{
-   baseShape = "./minable ore.dae";
-   adjustCenter = "1";
-   adjustFloor = "1";
-   loadLights = "0";
-};
--- Templates/FreeRPG/game/art/shapes/blendswap/mediaeval house.cs
+++
@@ -1,8 +0,0 @@
-
-singleton TSShapeConstructor(Mediaeval houseDae)
-{
-   baseShape = "./mediaeval house.dae";
-   adjustCenter = "1";
-   adjustFloor = "1";
-   loadLights = "0";
-};
