a69415079db1955d299c60a7930ef34491fac14d
Michael Haslam <hreikin@gmail.com>
2015-08-22 17:30:33 +0100

    All levels populated with a basic set of props/buildings, see Issue #3

---
+++ Templates/FreeRPG/game/art/shapes/Ruins/TempleRuin_01/TempleRuin_01.cs
@@ -0,0 +1,8 @@
+
+singleton TSShapeConstructor(TempleRuin_01DAE)
+{
+   baseShape = "./TempleRuin_01.DAE";
+   adjustCenter = "1";
+   adjustFloor = "1";
+   loadLights = "0";
+};
---
+++ Templates/FreeRPG/game/art/shapes/Ruins/TempleRuin_02/TempleRuin_02.cs
@@ -0,0 +1,8 @@
+
+singleton TSShapeConstructor(TempleRuin_02DAE)
+{
+   baseShape = "./TempleRuin_02.DAE";
+   adjustCenter = "1";
+   adjustFloor = "1";
+   loadLights = "0";
+};
